a4a2bf5634c3409be004736ca4df53689fe06608
wright1 <prosperwright@yahoo.co.uk>
2019-09-03 23:06:20 +0100

    initial commit

---
+++ src/components/homepg/homepg.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Section = styled.section`
+height: 100vh;
+width: 100vw;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+background-color: #fffaea;
+font-family: 'Oswald', sans-serif;
+font-size: 3em;
+`
+
+
+
+
+const HomePage = () => {
+    return(
+        <Section>
+            Mealplanz
+        </Section>
+
+    )
+    
+}
+
+export default HomePage
---
+++ src/components/shared/nav.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+const Div = styled.div`
+width: 100vw;
+height: 10vh;
+display: flex;
+justify-content: center;
+
+`
+
+
+const Nav = () => {
+    return(
+        <Div>
+            cats
+            
+
+        </Div>
+
+    )}
+
+export default Nav
